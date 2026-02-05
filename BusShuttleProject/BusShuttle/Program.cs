736bc5a98f63343b0bda1986903cf972c764cdce
damong31 <ddgardnerjr@gmail.com>
2026-02-05 05:10:34 +0000

    Draft

--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -1,9 +1,34 @@
 ﻿namespace BusShuttle;
 
+
+using System.IO;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Select Mode (Driver or Manager): ");
+        string mode = Console.ReadLine();
+
+        if(mode == "Driver")  {
+            
+            string command;
+
+            do {
+                
+                Console.Write("Enter Stop Name: ");
+
+                Console.Write("Enter number of boarded passengers: ");
+                int boarded = int.Parse(Console.ReadLine());
+
+                File.AppendAllText("passenger-data.txt" ,stopName+":"+boarded+Environment.NewLine);
+
+                Console.Write("Enter Command (end OR continue): ");
+                command = Console.ReadLine();
+
+
+
+
+        } while(command!="end");
     }
 }
